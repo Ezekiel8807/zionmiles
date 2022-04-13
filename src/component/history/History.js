d37d520555042ed160db1a9994a4b0a0a9fc33f7
Ayebidun Ezekiel <ayeoluwaseyi@gmail.com>
2022-04-13 12:47:41 +0100

    new commit

--- src/component/history/History.js
+++ src/component/history/History.js
@@ -3,22 +3,24 @@ import './history.css'
 const History = () => {
     return(
         <div className="history">
-            <div className='col-md-4'>
-                <div className='image-con'>
-                    <img src='/asset/img/f.jpeg' alt='me'/>
-                </div>
-            </div><br/>
+            <div className='row'>
+                <div className='col-md-4'>
+                    <div className='image-con'>
+                        <img src='/asset/img/f.jpeg' alt='me'/>
+                    </div>
+                </div><br/>
 
-            <div className='col-md-4'>
-                <div className='image-con'>
-                    <img src='/asset/img/g.jpeg' alt='you'/>
-                </div>
-            </div><br/>
+                <div className='col-md-4'>
+                    <div className='image-con'>
+                        <img src='/asset/img/g.jpeg' alt='you'/>
+                    </div>
+                </div><br/>
 
-            <div className='col-md-4'>
-                <div className='image-con'>
-                <img src='/asset/img/e.jpeg' alt='wallet'/>
-                </div>
+                <div className='col-md-4'>
+                    <div className='image-con'>
+                    <img src='/asset/img/e.jpeg' alt='wallet'/>
+                    </div>
+                </div><br/>
             </div><br/>
         </div> 
     )
