d99dd4df3603b10bc88cc128dccfb861d778200c
Ayebidun Ezekiel <ayeoluwaseyi@gmail.com>
2022-04-13 11:35:00 +0100

    another change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import './app.css';
 import Header from "./component/header/Header";
 import Footer from "./component/footer/Footer";
 import About from "./component/about/About";
 import Button from "./component/button/Button";
+import History from './component/history/History';
 
 function App() {
   return (
     <div className="app">
       <Header/>
         <div className="container">
-          <About />
+          <h2>Making money hasnt be easy but guess <b>what?</b></h2>
+
+          <p>I have a recipe that has made me and my students make over <b>100k - 500K</b> in less than a month</p>
+
+          <p>This strategy works for anyone and can work for you too, even if:</p>
+
           <Button />
+
+          <ul>
+            <li>No experience</li>
+            <li>No laptops</li>
+            <li>No products to sell</li>
+          </ul>
+
+          <Button />
+
+          <About />
+
+          <Button />
+          
+          <History />
+
+          <Button />
+
         </div>
       <Footer/>
     </div>
--- src/component/about/About.js
+++ src/component/about/About.js
@@ -16,9 +16,12 @@ const About = () =>{
 
                 <div className='col-md-6'>
                     <div className='summary'>
-                        <h3>Hi,</h3>
-                        <h3>I'm Folorunsho Michaels</h3>
-                        <p></p>
+                        <h6>Hi👋</h6>
+                        <h6>I'm Folorunsho Michael,</h6>
+                        <p> popular known as zion miles I am an affiliate marketer and a tutor in the field of online marketing 
+                            Have been able successfully made incredibly progress in marketing and am ready to teach willing 
+                            folks on how to repeat the same process and cross 6figures in very short time
+                        </p>
                     </div>
                 </div>
             </div>
