354218958e2d1a7a57b0b8046517831c78d652a2
Ayebidun Ezekiel <ayeoluwaseyi@gmail.com>
2022-04-14 10:41:22 +0100

    add whatapp link

--- src/component/button/Button.js
+++ src/component/button/Button.js
@@ -3,7 +3,7 @@ import './button.css'
 const Button = () => {
     return(
 
-        <a className="whatapplink" href="/">
+        <a className="whatapplink" href="https://wa.link/1ykt13">
             <div className="w">
                 <h5>CLICK TO GET ACCESS TO FREE VIDEO</h5>
                 <p>Limited slots avaliable</p>
